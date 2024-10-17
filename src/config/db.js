f686e64e7c8aa4a5fb858e7847ba2854f6e9f746
LeonardoAlmeidaRibas <leo.almeidaribas21@gmail.com>
2024-10-17 19:39:23 -0300

    att

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}.mongodb.net/`
-            // `mongoose.connect('mongodb+srv://<DB_USER>:<PASSWORD>i@<CLUSTER_NAME>.mongodb.net/');`
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+            // `mongoose.connect('mongodb+srv://<DB_USER>:<PASSWORD>i@<CLUSTER_NAME>/<COLLECTION_NAME>');`
           );
-          console.log("Conectado ao Mongo DB")
 
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
